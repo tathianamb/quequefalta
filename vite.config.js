5a4eddfe1a3997139e5ac8794d5050078def4801
Tathiana Barchi <57972124+tathianamb@users.noreply.github.com>
2026-05-05 23:57:16 -0300

    fix: nome dos arquivos de icone

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         start_url: '/quequefalta/',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'qqf-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'qqf-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
