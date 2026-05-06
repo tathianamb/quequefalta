73b50583f2bc8657a5c3d63b81359431e966d7a2
Tathiana Barchi <57972124+tathianamb@users.noreply.github.com>
2026-05-06 11:55:15 -0300

    fix: dominio

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "tathianamb.github.io",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
